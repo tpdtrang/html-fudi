e05123a4afdc2fab8b8a8638a64e8ea4a13f8fb6
tpdtrang <tpdtrang97@gmail.com>
2019-03-11 14:14:49 +0700

    Push feature/banner

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(windown).scrollTop();
         var opacity = 0;
-        if(scroll<100){
-            opacity = scroll/100;
+        if (scroll < 100) {
+            opacity = scroll / 100;
             $("#p-page-header").css('background-color', 'rgba(75, 193,210,' + opacity + ')');
 
         }
-        else{
+        else {
             opacity = 1;
             $("#p-page-header").css('background-color', 'rgba(75, 193,210,' + opacity + ')');
         }
