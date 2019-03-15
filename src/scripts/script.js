669f36a9df0bd2377817e93e9e6ea3968c65fe67
tpdtrang <tpdtrang97@gmail.com>
2019-03-15 15:36:51 +0700

    Push feature/wow

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,3 +15,14 @@ $(document).ready(function () {
     })
 });
 
+// wow
+wow = new WOW(
+	{
+			animateClass: 'animated',
+			offset: 100,
+			callback: function (box) {
+					console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+			}
+	}
+);
+wow.init();
